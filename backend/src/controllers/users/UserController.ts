92bf03f5fa4f9fbc64a2563e572f9039445c5bb5
devramon <dsramondev@gmail.com>
2025-08-19 11:36:03 -0300

    feat: todas funcionalidades do crud do nosso usuário desenvolvida dentro do controlador

---
+++ backend/src/controllers/users/UserController.ts
@@ -0,0 +1,64 @@
+import { Request, Response } from "express";
+import { UserModel } from "../../models/users/UserModel.js";
+
+class UserController {
+
+  // creating new user
+  async create(req: Request, res: Response) {
+    const { nome, email, senha } = req.body;
+
+    const userModel = new UserModel();
+
+    const createUserModel = await userModel.create({
+      nome,
+      email,
+      senha,
+    });
+
+    return res.json(createUserModel);
+  }
+
+
+  // listing users
+  async getUsers(req: Request, res: Response) {
+      const userModel = new UserModel();
+      const getUserModel = await userModel.getUsers();
+
+      return res.json(getUserModel);
+  }
+
+  // list user by id
+  async getUsersByID(req: Request, res: Response) {
+    const {id} = req.params;
+
+    const userModel = new UserModel();
+    const getUserId = await userModel.getUsersByID({ id: Number(id) });
+
+    return res.json(getUserId);
+  }
+
+  // atualizar usuario
+  async updateUser(req: Request, res: Response){ 
+    // pega o id da url
+    const {id} = req.params;
+
+    const {nome, email, senha} = req.body;
+
+    const updateUserModel = new UserModel();
+    const updateUser = updateUserModel.updateUser(Number(id), {nome, email, senha})
+
+    return res.json(updateUser);
+  }
+
+  // deletar usuário
+  async deleteUser(req: Request, res: Response){ 
+    const {id} = req.params;
+
+    const userModel = new UserModel();
+    await userModel.deletarUsuario({id: Number(id)});
+
+    return res.status(204).end();
+  }
+}
+
+export { UserController };

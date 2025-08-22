be48817383dd1b2d50334b705010019e6aa190bd
devramon <dsramondev@gmail.com>
2025-08-22 17:55:53 -0300

    feat: rota de autenticação

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -2,6 +2,7 @@ import {Router} from 'express';
 
 // Usuarios
 import {UserController} from './controllers/users/UserController.js';
+import {login} from './controllers/auth/login.js'; 
 
 const router = Router();
 
@@ -12,5 +13,8 @@ router.get('/users/:id', new UserController().getUsersByID);
 router.put('/users/:id', new UserController().updateUser);
 router.delete('/users/:id', new UserController().deleteUser);
 
+
+// O Login 
+router.post('/auth/login', login);
 
 export { router };
